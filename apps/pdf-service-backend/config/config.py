9889f76bb6d52ba856efcc7c528b4f568ce4efe2
sd1p <skmxyt@gmail.com>
2026-01-20 00:32:51 +0530

    refactor: update backend configuration and dependencies

--- apps/pdf-service-backend/config/config.py
+++ apps/pdf-service-backend/config/config.py
@@ -3,19 +3,20 @@ import boto3
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 from supabase import create_client, Client
+
 load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")    
+AWS_REGION = os.getenv("AWS_REGION")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 APP_FRONTEND_URL = os.getenv("APP_FRONTEND_URL")
 
-SUPABASE_URL=os.getenv("SUPABASE_URL")
-SUPABASE_KEY=os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -26,6 +27,3 @@ session = boto3.Session(
 )
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-
-
